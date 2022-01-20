0ac9cde12d45d24eac816534f6e8a8dfedef30be
amirbahador.pv <amirbahador.pv@gmail.com>
2022-01-20 06:33:26 -0500

    Extract request line

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ LOG_File_Name=logs
 
 # awk -F\" '{print $4}' $LOG_File_Name    # referer  
 # awk -F\" '{print $6}' $LOG_File_Name    # user agent
-awk '{print $1}' $LOG_File_Name           # ip address 
+# awk '{print $1}' $LOG_File_Name           # ip address 
+awk -F\" '{print $2}' $LOG_File_Name    # request line (%r)
